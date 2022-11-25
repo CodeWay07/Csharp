0191bff4a295eef23ce9c02061bd8872f1e0959d
CodeWay07 <maxim.bugaescu.mb@gmail.com>
2022-11-25 17:09:18 +0200

    Csharp commit

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,2 +1,41 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TestConsole
+{
+    class Program
+    {         
+        static void Main(string[] args)
+        {
+            int x = 6;
+            int y = ++x;
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
